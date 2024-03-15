ec6ef4c67602d7d8705d0f9d1b1de3e6156ebf58
RAKIB CHOWDHURY <152077471+SYSTEM-ERROR-KING@users.noreply.github.com>
2024-03-16 00:12:42 +0600

    Update kickall.js

--- scripts/commands/kickall.js
+++ scripts/commands/kickall.js
@@ -10,15 +10,25 @@ module.exports.config = {
   prefix: true
 };
 
-module.exports.run = async function({ api, event, args }) {
-  var threadInfo = await api.getThreadInfo(event.threadID)
-  var id = threadInfo.participantIDs
-  const user = args.join(" ")
+module.exports.run = async function({ api, event, getText,args }) {
+  const { participantIDs } = await api.getThreadInfo(event.threadID)
   function delay(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
   };
-  for (let user of id) {
-    await delay(5000)
-    api.removeUserFromGroup(user, event.threadID, user);
-  }
-};
+  const botID = api.getCurrentUserID();
+  const listUserID = participantIDs.filter(ID => ID != botID);
+  return api.getThreadInfo(event.threadID, (err, info) => {
+    if (err) return api.sendMessage("» An error occurred.", event.threadID);
+    if (!info.adminIDs.some(item => item.id == api.getCurrentUserID()))
+      return api.sendMessage(`» Need group admin rights.\nPlease add and try again.`, event.threadID, event.messageID);
+    if (info.adminIDs.some(item => item.id == event.senderID)) {
+      setTimeout(function() { api.removeUserFromGroup(botID, event.threadID) }, 300000);
+      return api.sendMessage(`» সমস্ত সদস্য মুছে ফেলা শুরু করুন। বিদায় সবাই..`, event.threadID, async (error, info) => {
+        for (let id in listUserID) {
+          await new Promise(resolve => setTimeout(resolve, 1000));
+          api.removeUserFromGroup(listUserID[id], event.threadID)
+        }
+      })
+    } else return api.sendMessage('» Only group admins can use this command.', event.threadID, event.messageID);
+  })
+}

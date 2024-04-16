6bd53cc447758b4c11b25913680d75773eb9a73d
RAKIB CHOWDHURY <152077471+SYSTEM-ERROR-KING@users.noreply.github.com>
2024-04-16 12:56:08 +0600

    Update random.js

--- scripts/commands/random.js
+++ scripts/commands/random.js
@@ -1,39 +1,3 @@
-module.exports.config = {
-  name: "random",
-	version: "0.0.2",
-	permission: 0,
-  prefix: 'awto',
-	credits: "Nayan",
-	description: "random video",
-	category: "user",
-	usages: "",
-    cooldowns: 5,
-};
-
-
-
-
-
-module.exports.run = async function({ api, event, args }) {
-    const axios = require("axios")
-    const request = require("request")
-    const fs = require("fs-extra")
-    const { NAYAN } = global.apiNayan;
-	const apis = await axios.get('https://raw.githubusercontent.com/MR-NAYAN-404/NAYAN-BOT/main/api.json')
-  const n = apis.data.api
-    const res = await axios.get(`${n}/video/mixvideo`);
-    var data = res.data.url;
-    var msg = [];
-    let video = `${res.data.url.url}`;
-  let name = `${res.data.url.name}`;
-    let cp = `${res.data.cp}`
-  let ln = `${res.data.length}`
-
-    let videos = (await axios.get(`${video}`, {
-        responseType: 'arraybuffer'
-    })).data;
-    fs.writeFileSync(__dirname + "/cache/video.mp4", Buffer.from(videos, "utf-8"));
-    var allimage = [];
     allimage.push(fs.createReadStream(__dirname + "/cache/video.mp4"));
 	
     {
